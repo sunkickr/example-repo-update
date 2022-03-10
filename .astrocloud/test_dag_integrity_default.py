abaff1a55142d217bbee94ba11b0271e62e9dcd3
sunkickr <davidkoenit@gmail.com>
2022-03-10 10:18:17 -0500

    updates

--- .astrocloud/test_dag_integrity_default.py
+++ .astrocloud/test_dag_integrity_default.py
@@ -37,8 +37,3 @@ def test_dagbag():
     dag_bag = DagBag(include_examples=False)
     print(dag_bag)
     assert not dag_bag.import_errors  # Import errors aren't raised but captured to ensure all DAGs are parsed
-
-    # Additional project-specific checks can be added here, e.g. to enforce each DAG has a tag
-    for dag_id, dag in dag_bag.dags.items():
-        error_msg = f"{dag_id} in {dag.fileloc} has no tags"
-        assert error_msg
