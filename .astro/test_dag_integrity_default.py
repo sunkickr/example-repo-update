c3e9d10493682e37b472c462d1f7ee02616ef46a
sunkickr <davidkoenit@gmail.com>
2022-09-12 13:42:16 -0500

    update

--- .astro/test_dag_integrity_default.py
+++ .astro/test_dag_integrity_default.py
@@ -23,9 +23,13 @@ BaseHook.get_connection = basehook_get_connection_monkeypatch
 # =========== MONKEYPATCH OS.GETENV() ===========
 # def os_getenv_monkeypatch(key: str, *args, default=None, **kwargs):
 # 	print(f"Attempted to fetch os environment variable during parse, returning a mocked value for {key}")
+# 	if key == 'JENKINS_HOME' and default is None: # fix https://github.com/astronomer/astro-cli/issues/601
+# 		return None
 # 	if default:
 # 		return default
 # 	return "NON_DEFAULT_OS_ENV_VALUE"
+
+
 # os.getenv = os_getenv_monkeypatch
 # # =========== /MONKEYPATCH OS.GETENV() ===========
 
---
+++ dags/basic_sql_dag.py
@@ -0,0 +1,40 @@
+"""
+basic sql dag
+"""
+import json
+from datetime import datetime
+
+from airflow.decorators import dag
+from astro import sql as aql
+from airflow.decorators import dag, task
+
+@aql.run_raw_sql(conn_id='david_snowflake')
+def run_sql():
+    return """select * from REDDIT_AIRFLOW;
+    """
+
+@task()
+def basic_python():
+    """
+    #### Extract task
+    A simple "extract" task to get data ready for the rest of the
+    pipeline. In this case, getting data is simulated by reading from a
+    hardcoded JSON string.
+    """
+    data_string = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
+
+    order_data_dict = json.loads(data_string)
+    print(order_data_dict)
+
+    total_order_value = 0
+
+    for value in order_data_dict.values():
+            total_order_value += value
+            print(total_order_value)
+
+
+@dag(schedule_interval="@daily", start_date=datetime(2022, 7, 27), catchup=False, tags=[])
+def basic_sql():
+     basic_python() >> run_sql()
+
+dag_obj = basic_sql()
