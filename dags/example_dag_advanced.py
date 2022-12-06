02bdb811f15443322d2c46789eeaaa65c157bd9f
sunkickr <davidkoenit@gmail.com>
2022-12-05 21:04:27 -0500

    update dag

--- dags/example_dag_advanced.py
+++ dags/example_dag_advanced.py
@@ -62,35 +62,12 @@ BranchDayOfWeekOperator -
         https://registry.astronomer.io/providers/apache-airflow/modules/branchdayofweekoperator
 """
 
-# # Reference data that defines "weekday" as well as the activity assigned to each day of the week.
-# DAY_ACTIVITY_MAPPING = {
-#     "monday": {"is_weekday": True, "activity": "guitar lessons"},
-#     "tuesday": {"is_weekday": True, "activity": "studying"},
-#     "wednesday": {"is_weekday": True, "activity": "soccer practice"},
-#     "thursday": {"is_weekday": True, "activity": "contributing to Airflow"},
-#     "friday": {"is_weekday": True, "activity": "family dinner"},
-#     "saturday": {"is_weekday": False, "activity": "going to the beach"},
-#     "sunday": {"is_weekday": False, "activity": "sleeping in"},
-# }
-
-
 @task(multiple_outputs=True) # multiple_outputs=True unrolls dictionaries into separate XCom values
 def _going_to_the_beach() -> Dict:
     return {
         "subject": "Beach day!",
         "body": "It's Saturday and I'm heading to the beach.<br><br>Come join me!<br>",
     }
-
-
-# # This functions gets the activity from the "DAY_ACTIVITY_MAPPING" dictionary
-# def _get_activity(day_name) -> str:
-#     activity_id = DAY_ACTIVITY_MAPPING[day_name]["activity"].replace(" ", "_")
-
-#     if DAY_ACTIVITY_MAPPING[day_name]["is_weekday"]:
-#         return f"weekday_activities.{activity_id}"
-
-#     return f"weekend_activities.{activity_id}"
-
 
 # When using the DAG decorator, the "dag" argument doesn't need to be specified for each task.
 # The "dag_id" value defaults to the name of the function it is decorating if not explicitly set.
